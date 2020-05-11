91fd97903cdfda301deb7ab2b15587c0b2553e9f
Krakenos <1944193+Krakenos@users.noreply.github.com>
2020-05-11 16:46:49 +0200

    Alusz003 backend front endpoints (#9)
    Updated models
    Updated rest api endpoints
    Improved project workspace on the site
    Small bugfixes

--- backend/pznsi/serializers.py
+++ backend/pznsi/serializers.py
@@ -1,57 +1,12 @@
 from rest_framework import serializers
 
-from pznsi.models import Environment, Project, Comment, User, Attachment, Vote
-
+from pznsi.models import Environment, Project, Comment, User
 
 class UserSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = User
-        fields = ['id', 'username', 'first_name', 'avatar']
-
-
-class CommentSerializer(serializers.ModelSerializer):
-    user = UserSerializer(read_only=True)
-    date = serializers.DateTimeField(source='comment_date')
-
-    class Meta:
-        model = Comment
-        fields = ['user', 'comment_title', 'comment_content', 'comment_reaction', 'date']
-
-
-class AttachmentSerializer(serializers.ModelSerializer):
-    user = UserSerializer(read_only=True)
-    date = serializers.DateTimeField(source='attachment_creation_date')
-
-    class Meta:
-        model = Attachment
-        fields = ['user', 'content', 'date']
-
-
-class VoteSerializer(serializers.ModelSerializer):
-    user = UserSerializer(read_only=True)
-
-    # TODO add averages methodfield
-    class Meta:
-        model = Vote
-        fields = ['user', 'vote_date', 'vote_content']
-
-
-class ProjectDetailSerializer(serializers.ModelSerializer):
-    comments = CommentSerializer(source='comment_set', many=True, required=False)
-    environment_name = serializers.SlugRelatedField(source='environment', slug_field='environment_name', read_only=True)
-    owner = UserSerializer(read_only=True)
-    attachments = AttachmentSerializer(source='attachment_set', many=True, required=False, read_only=True)
-    votes = VoteSerializer(source='vote_set', many=True, required=False, read_only=True)
-
-    class Meta:
-        model = Project
-        fields = ['id', 'project_name', 'project_status', 'project_category', 'project_content', 'comments',
-                  'cover_image', 'environment', 'environment_name', 'owner', 'attachments', 'votes']
-
-    def create(self, validated_data):
-        validated_data.pop('comment_set', None)
-        project = Project.objects.create(**validated_data)
-        return project
+        fields = ['id', 'username', 'first_name']
 
 
 class ProjectBasicsSerializers(serializers.ModelSerializer):
@@ -62,8 +17,30 @@ class ProjectBasicsSerializers(serializers.ModelSerializer):
         fields = ['id', 'project_name', 'owner', 'project_content', 'cover_image']
 
 
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ['user', 'comment_title', 'comment_content', 'comment_reaction', 'comment_date']
+
+
+class ProjectDetailSerializer(serializers.ModelSerializer):
+    comments = CommentSerializer(source='comment_set', many=True, required=False)
+    environment_name = serializers.StringRelatedField(source='environment')
+
+    class Meta:
+        model = Project
+        fields = ['id', 'project_name', 'project_status', 'project_category', 'project_content', 'comments', 'owner',
+                  'cover_image', 'environment', 'environment_name']
+
+    def create(self, validated_data):
+        validated_data.pop('comment_set', None)
+        project = Project.objects.create(**validated_data)
+        return project
+
+
+
 class EnvironmentSerializer(serializers.ModelSerializer):
-    projects = serializers.SerializerMethodField()
+    projects = ProjectBasicsSerializers(source='project_set', many=True, required=False)
     owner = UserSerializer(read_only=True)
 
     class Meta:
@@ -75,6 +52,4 @@ class EnvironmentSerializer(serializers.ModelSerializer):
         environment = Environment.objects.create(**validated_data)
         return environment
 
-    def get_projects(self, obj):
-        projects = obj.get_projects(self.context['request'].user)
-        return ProjectBasicsSerializers(projects, many=True).data
+
