04cdc630e5e8e408f9a2591611aecbe24a0f470b
Krakenos <1944193+Krakenos@users.noreply.github.com>
2020-05-19 21:09:35 +0200

    Dszczu004 backend permissions (#11)
    * edited urls and added guardian
    
    * added project permissions and groups
    
    * added environment permissions
    
    * rest api filtering based on permissions
    
    * filtering anonymoususer
    
    * adding permission support to front endpoints
    
    * fixed front endpoint and optimized imports
    
    * mistake
    
    * fixing owner field
    
    * lots of changes and new api endpoints
    
    * typo
    
    * change field name
    
    * front endpoints
    
    * typo
    
    * added rest endpoint for project permissions
    
    * typos
    
    * fixes
    
    * migration conflicts
    
    * changelog

--- backend/pznsi/admin.py
+++ backend/pznsi/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import User, Environment, Project, Comment, ProjectCategory
+from .models import User, Environment, Project, Comment, ProjectCategory, Attachment
 
 admin.site.register(User)
 admin.site.register(Environment)
 admin.site.register(Project)
 admin.site.register(Comment)
+admin.site.register(Attachment)
 admin.site.register(ProjectCategory)
