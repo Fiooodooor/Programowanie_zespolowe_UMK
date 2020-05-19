65b01da017301f46e8f78c7c6fc01f990cadd1bb
Krakenos <1944193+Krakenos@users.noreply.github.com>
2020-05-19 21:09:35 +0200

    Dszczu004 backend permissions (#11)
    * edited urls and added guardian
    
    * added project permissions and groups
    
    * added environment permissions
    
    * rest api filtering based on permissions
    
    * filtering anonymoususer
    
    * adding permission support to front endpoints
    
    * fixed front endpoint and optimized imports
    
    * mistake
    
    * fixing owner field
    
    * lots of changes and new api endpoints
    
    * typo
    
    * change field name
    
    * front endpoints
    
    * typo
    
    * added rest endpoint for project permissions
    
    * typos
    
    * fixes
    
    * migration conflicts
    
    * changelog

--- backend/pznsi/serializers.py
+++ backend/pznsi/serializers.py
@@ -1,12 +1,57 @@
 from rest_framework import serializers
 
-from pznsi.models import Environment, Project, Comment, User
+from pznsi.models import Environment, Project, Comment, User, Attachment, Vote
+
 
 class UserSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = User
-        fields = ['id', 'username', 'first_name']
+        fields = ['id', 'username', 'first_name', 'avatar']
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+    date = serializers.DateTimeField(source='comment_date')
+
+    class Meta:
+        model = Comment
+        fields = ['user', 'comment_title', 'comment_content', 'comment_reaction', 'date']
+
+
+class AttachmentSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+    date = serializers.DateTimeField(source='attachment_creation_date')
+
+    class Meta:
+        model = Attachment
+        fields = ['user', 'content', 'date']
+
+
+class VoteSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+
+    # TODO add averages methodfield
+    class Meta:
+        model = Vote
+        fields = ['user', 'vote_date', 'vote_content']
+
+
+class ProjectDetailSerializer(serializers.ModelSerializer):
+    comments = CommentSerializer(source='comment_set', many=True, required=False)
+    environment_name = serializers.SlugRelatedField(source='environment', slug_field='environment_name', read_only=True)
+    owner = UserSerializer(read_only=True)
+    attachments = AttachmentSerializer(source='attachment_set', many=True, required=False, read_only=True)
+    votes = VoteSerializer(source='vote_set', many=True, required=False, read_only=True)
+
+    class Meta:
+        model = Project
+        fields = ['id', 'project_name', 'project_status', 'project_category', 'project_content', 'comments',
+                  'cover_image', 'environment', 'environment_name', 'owner', 'attachments', 'votes']
+
+    def create(self, validated_data):
+        validated_data.pop('comment_set', None)
+        project = Project.objects.create(**validated_data)
+        return project
 
 
 class ProjectBasicsSerializers(serializers.ModelSerializer):
@@ -17,29 +62,8 @@ class ProjectBasicsSerializers(serializers.ModelSerializer):
         fields = ['id', 'project_name', 'owner', 'project_content', 'cover_image']
 
 
-class CommentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Comment
-        fields = ['user', 'comment_title', 'comment_content', 'comment_reaction', 'comment_date']
-
-
-class ProjectDetailSerializer(serializers.ModelSerializer):
-    comments = CommentSerializer(source='comment_set', many=True, required=False)
-    environment_name = serializers.StringRelatedField(source='environment')
-
-    class Meta:
-        model = Project
-        fields = ['id', 'project_name', 'project_status', 'project_category', 'project_content', 'comments', 'owner',
-                  'cover_image', 'environment', 'environment_name']
-
-    def create(self, validated_data):
-        validated_data.pop('comment_set', None)
-        project = Project.objects.create(**validated_data)
-        return project
-
-
 class EnvironmentSerializer(serializers.ModelSerializer):
-    projects = ProjectBasicsSerializers(source='project_set', many=True, required=False)
+    projects = serializers.SerializerMethodField()
     owner = UserSerializer(read_only=True)
 
     class Meta:
@@ -51,4 +75,6 @@ class EnvironmentSerializer(serializers.ModelSerializer):
         environment = Environment.objects.create(**validated_data)
         return environment
 
-
+    def get_projects(self, obj):
+        projects = obj.get_projects(self.context['request'].user)
+        return ProjectBasicsSerializers(projects, many=True).data
--- backend/pznsi/urls.py
+++ backend/pznsi/urls.py
@@ -2,9 +2,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth.views import LogoutView, LoginView
 from django.urls import path, include
-from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework import routers
-
+from rest_framework.authtoken.views import obtain_auth_token
 
 from . import views
 from .views import Environments, Projects
@@ -13,31 +12,28 @@ router = routers.DefaultRouter()
 router.register(r'environments', Environments)
 router.register(r'projects', Projects)
 
-
 urlpatterns = [
-    path('api/auth/', obtain_auth_token, name='auth'),
     path('', views.index, name='index'),
-    path('workspace/', views.workspace, name='workspace'),
     path('login/', LoginView.as_view(redirect_authenticated_user=True), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('register/', views.register, name='register'),
     path('main/', views.main_page, name='main'),
+    path('workspace/', views.workspace, name='workspace'),
     path('profile/edit/', views.edit_profile, name='edit_profile'),
-    path('register/', views.register, name='register'),
-    path('api/', include(router.urls)),
-    path('register/', views.register, name='register'),
     path('front/environments/', views.front_environments, name='front_environments'),
     path('front/projects/', views.front_projects, name='front_projects'),
     path('front/editEnvi', views.edit_environment, name='editEnvi'),
     path('front/editProject', views.edit_project, name='editProject'),
+    path('front/environmentsperms/', views.PermEnviroment),
+    path('front/projectperms/', views.permProject),
+    path('front/project/', views.project),
+    path('api/', include(router.urls)),
+    path('api/auth/', obtain_auth_token, name='auth'),
     path('api/editEnviSave', views.save_environment, name='saveEnvi'),
     path('api/editprojectSave', views.save_project, name='saveProject'),
     path('api/canAddEnvi', views.can_add_envi, name='canAddEnvi'),
-    path('api/canAddProject', views.can_add_project, name='canAddProject'),
-    path('front/environmentsperms/', views.PermEnviroment),
-    path('front/projectperms/', views.permProject),
-    path('front/project/', views.project)
+    path('api/canAddProject', views.can_add_project, name='canAddProject')
 ]
-
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
