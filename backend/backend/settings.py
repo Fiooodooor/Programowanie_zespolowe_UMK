568b687936981ab5b69f5ab62402d22f9bdc8f13
Krakenos <1944193+Krakenos@users.noreply.github.com>
2020-05-24 01:04:11 +0200

    Dszczu006 backend repositories (#14)
    * changing permissions
    
    * implemented repository
    
    * new front
    
    * lots of small changes
    
    * new front version
    
    * datetime fix
    
    * queryset fix
    
    * another fix
    
    * added functions to models
    
    * name change
    
    * small backend fixes
    
    * new front + small bugfixes
    
    * oops

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,8 @@ env = environ.Env(
     STATIC_URL=(str, '/static/'),
     STATIC_ROOT=(str, ''),
     MEDIA_URL=(str, '/media/'),
-    MEDIA_ROOT=(str, os.path.join(BASE_DIR, 'media'))
+    MEDIA_ROOT=(str, os.path.join(BASE_DIR, 'media')),
+    USE_TZ=(bool, False)
 )
 
 # reading .env file
@@ -146,7 +147,7 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = env('USE_TZ')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
--- backend/pznsi/models.py
+++ backend/pznsi/models.py
@@ -1,9 +1,17 @@
+import os
+from datetime import timedelta
+
 from django.contrib.auth.models import AbstractUser, Group
+from django.core.files.base import ContentFile
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.utils.datetime_safe import datetime
-from guardian.shortcuts import assign_perm, get_objects_for_user
+from guardian.shortcuts import assign_perm
+
+
+def get_default_time():
+    return datetime.now() + timedelta(days=14)
 
 
 class User(AbstractUser):
@@ -30,8 +38,8 @@ class Environment(models.Model):
     def __str__(self):
         return f'{self.environment_name} ({self.id})'
 
-    def get_projects(self, user):
-        return get_objects_for_user(user, 'view_project_instance', self.project_set)
+    def get_projects(self):
+        return self.project_set
 
 
 class ProjectCategory(models.Model):
@@ -43,7 +51,8 @@ class Project(models.Model):
     project_status = models.CharField(blank=True, max_length=100)
     project_password = models.CharField(max_length=128)
     project_creation_date = models.DateField(blank=True, null=True)
-    project_closing_date = models.DateField(blank=True, null=True)
+    vote_starting = models.DateTimeField(default=datetime.now)
+    vote_closing = models.DateTimeField(default=get_default_time)
     project_content = models.CharField(blank=True, max_length=1000)
     project_category = models.ForeignKey(ProjectCategory, on_delete=models.CASCADE, null=True, blank=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
@@ -72,7 +81,6 @@ class Comment(models.Model):
 
 class Attachment(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE, null=True, blank=True)
-    repository = models.ForeignKey('Repository', on_delete=models.CASCADE, null=True, blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     comment = models.ForeignKey(Comment, on_delete=models.CASCADE, null=True, blank=True)
     attachment_name = models.CharField(max_length=2000, null=True, blank=True)
@@ -80,6 +88,10 @@ class Attachment(models.Model):
     attachment_creation_date = models.DateTimeField(default=datetime.now, blank=True, null=True)
     attachment_type = models.CharField(max_length=50, default='file')
     attachment_visible_date = models.DateField(blank=True, null=True)
+
+    def extension(self):
+        name, extension = os.path.splitext(self.content.name)
+        return extension
 
 
 class Vote(models.Model):
@@ -89,10 +101,26 @@ class Vote(models.Model):
     vote_content = models.IntegerField(null=True, blank=True)
 
 
-class Repository(models.Model):
-    repository_file_content = models.CharField(max_length=2000, blank=True, null=True)
-    repository_file_date_created = models.DateTimeField(blank=True, null=True)
-    repository_file_status = models.CharField(max_length=100, blank=True, null=True)
+class RepositoryFile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    file = models.FileField(upload_to='repository_files', blank=True, null=True)
+    visible_name = models.CharField(max_length=200, blank=True, null=True)
+    file_date_created = models.DateTimeField(default=datetime.now)
+    status = models.CharField(max_length=100, blank=True, null=True)
+
+    def copy_to_project(self, project):
+        new_attachment = Attachment()
+        new_attachment.project = project
+        new_attachment.user = self.user
+        new_attachment.attachment_name = self.visible_name
+        new_file = ContentFile(self.file.read())
+        new_file.name = self.file.name
+        new_attachment.content = new_file
+        new_attachment.save()
+
+    def extension(self):
+        name, extension = os.path.splitext(self.file.name)
+        return extension
 
 
 @receiver(post_save, sender=Project)
@@ -109,13 +137,15 @@ def project_post_save(sender, **kwargs):
         viewers, _ = Group.objects.get_or_create(name=viewers_name)
         editors, _ = Group.objects.get_or_create(name=editors_name)
         voters, _ = Group.objects.get_or_create(name=voters_name)
+        environment_viewers = Group.objects.get(name=f'{project.environment.id}_environment_viewers')
         assign_perm('view_project_instance', viewers, project)
         assign_perm('edit_project_instance', editors, project)
         assign_perm('vote', voters, project)
         if project.owner is not None:
             project.owner.groups.add(viewers)
             project.owner.groups.add(editors)
-            project.owner.groups.add(voters)  # TODO should owner be able to vote?
+            project.owner.groups.add(voters)
+            project.owner.groups.add(environment_viewers)
 
 
 @receiver(post_save, sender=Environment)
